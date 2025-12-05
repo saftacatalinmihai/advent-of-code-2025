13a7bd156da729aea9898d1cad832c4a85172d0c
Catalin Mihai Safta <catalin-mihai.safta@ing.com>
2025-12-05 20:09:39 +0200

    cleanup

--- day5.c
+++ day5.c
@@ -8,28 +8,28 @@ typedef struct Range { long start; long end; bool merged; } Range;
 
 int main () {
     FILE *fp = fopen("day5input.txt", "rb");
-    Range ranges[512] = {0};
+    Range range[512] = {0};
     long ids[2048] = {0};
     long a = 0; long b = 0;
     bool range_section = true;
-    int ranges_count = 0, ids_count = 0;
+    int range_count = 0, ids_count = 0;
     for (char input[256]; fgets(input, sizeof(input), fp);) {
         if (range_section) {
             int num_matched = sscanf(input, "%ld-%ld", &a, &b);
             if (num_matched > 0) {
-                for (int i = 0; i < ranges_count; ++i) {
-                    if (ranges[i].merged) continue;
-                    if ((a >= ranges[i].start && a <= ranges[i].end) ||
-                        (b >= ranges[i].start && b <= ranges[i].end) ||
-                        (a <= ranges[i].start && b >= ranges[i].end)) {
-                        ranges[i].merged = true;
-                        a = a < ranges[i].start ? a : ranges[i].start;
-                        b = b > ranges[i].end ? b : ranges[i].end;
+                for (int i = 0; i < range_count; ++i) {
+                    if (range[i].merged) continue;
+                    if ((a >= range[i].start && a <= range[i].end) ||
+                        (b >= range[i].start && b <= range[i].end) ||
+                        (a <= range[i].start && b >= range[i].end)) {
+                        range[i].merged = true;
+                        a = a < range[i].start ? a : range[i].start;
+                        b = b > range[i].end ? b : range[i].end;
                     }
                 }
-                ranges[ranges_count].start = a;
-                ranges[ranges_count].end = b;
-                ranges_count++;
+                range[range_count].start = a;
+                range[range_count].end = b;
+                range_count++;
             } else range_section = false;
         } else {
             long id;
@@ -40,9 +40,9 @@ int main () {
 
     unsigned long count_fresh = 0;
     for (int i = 0; i < ids_count; ++i) {
-        for (int j = 0; j < ranges_count; ++j) {
-            if (ranges[j].merged) continue;
-            if (ids[i] >= ranges[j].start && ids[i] <= ranges[j].end) {
+        for (int j = 0; j < range_count; ++j) {
+            if (range[j].merged) continue;
+            if (ids[i] >= range[j].start && ids[i] <= range[j].end) {
                 count_fresh++;
                 break;
             }
@@ -51,10 +51,9 @@ int main () {
     printf("Count fresh IDs: %ld\n", count_fresh);
   
     count_fresh = 0;
-    for (int i = 0; i < ranges_count; ++i) {
-        if (ranges[i].merged) continue;
-        count_fresh += (ranges[i].end - ranges[i].start + 1);
+    for (int i = 0; i < range_count; ++i) {
+        if (range[i].merged) continue;
+        count_fresh += (range[i].end - range[i].start + 1);
     }
-
     printf("Count fresh IDs 2: %ld\n", count_fresh);
 }

6b09c565154678985e12d5f5b75bb576d4cbea48
Catalin Mihai Safta <catalin-mihai.safta@ing.com>
2025-12-08 11:08:34 +0200

    Day 7 part 2

--- day7.c
+++ day7.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-#include <string.h>
+
+typedef struct Cell {
+    enum {EMPTY, PATH, SPLIT} type;
+    long paths;
+} Cell;
 
 int main() {
     FILE *fp = fopen("day7input.txt", "rb");
-
     int line_number = 0, num_splits = 0;
     char input[256]; 
     fgets(input, sizeof(input), fp);
-
-    int diagram[256][256] = {0};
-    diagram[0][70] = 1;
+    Cell diagram[256][256] = {0};
+    diagram[0][70] = (Cell){.type=PATH};
     while(fgets(input, sizeof(input), fp)) { 
         for (int i = 0; i < 140; ++i) {
-            if (diagram[line_number][i] == 1) {
-                if (input[i] == '.') diagram[line_number+1][i] = 1;
+            if (diagram[line_number][i].type == PATH) {
+                if (input[i] == '.') diagram[line_number+1][i] = (Cell){.type = PATH};
                 if (input[i] == '^') {
-                    diagram[line_number+1][i - 1] = 1;
-                    diagram[line_number+1][i + 1] = 1;
+                    diagram[line_number+1][i] = (Cell){.type = SPLIT};
+                    diagram[line_number+1][i - 1] = (Cell){.type = PATH};
+                    diagram[line_number+1][i + 1] = (Cell){.type = PATH};
                     num_splits++;
                 }
             }
@@ -24,4 +27,17 @@ int main() {
         line_number++;
     }
     printf("Number of splits: %d\n", num_splits);
+
+    // Part 2
+    for (int i = 0; i<141; i++) { diagram[line_number][i].paths = 1; }
+    for (int i=line_number-1; i>=0; i--) {
+        for (int j=0; j<140; j++) {
+            if (diagram[i][j].type == SPLIT) {
+                diagram[i][j].paths = diagram[i+1][j-1].paths + diagram[i+1][j+1].paths;
+            } else if (diagram[i][j].type == PATH) {
+                diagram[i][j].paths = diagram[i+1][j].paths;
+            }
+        }
+    }
+    printf("Number of paths: %ld\n", diagram[0][70].paths);
 }

6ed10ce4eb59e1483d05992fe52ed32bd7cab2d9
Catalin Mihai Safta <catalin-mihai.safta@ing.com>
2025-12-25 16:17:01 +0200

    day9 part 1, WIP part 2

--- Makefile
+++ Makefile
@@ -3,36 +3,58 @@ default: all
 day1: day1.c
 	cc -o day1 day1.c
 
+day1_run: day1
+	time ./day1
+
 day2: day2.c
 	cc -o day2 day2.c
+
+day2_run: day2
+	time ./day2
 
 day3: day3.c
 	cc -o day3 day3.c
 
+day3_run: day3
+	time ./day3
+
 day4: day4.c
 	cc -o day4 day4.c
+
+day4_run: day4
+	time ./day4
 
 day5: day5.c
 	cc -o day5 day5.c
 
+day5_run: day5
+	time ./day5
+
 day6: day6.c
 	cc -o day6 day6.c
+
+day6_run: day6
+	time ./day6
 
 day7: day7.c
 	cc -o day7 day7.c
 
+day7_run: day7
+	time ./day7
+
 day8: day8.c
 	cc -o day8 day8.c
 
-all: day1 day2 day3 day4 day5 day6 day7 day8
-	time ./day1
-	time ./day2
-	time ./day3
-	time ./day4
-	time ./day5
-	time ./day6
-	time ./day7
+day8_run: day8
 	time ./day8
 
+day9: day9.c
+	cc -o day9 day9.c
+
+day9_run: day9
+	time ./day9
+
+all: day1_run day2_run day3_run day4_run day5_run day6_run day7_run day8_run day9_run
+
 clean:
-	rm -f day1 day2 day3 day4 day5 day6 day7 day8
+	rm -f day1 day2 day3 day4 day5 day6 day7 day8 day9
---
+++ day9.c
@@ -0,0 +1,68 @@
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* #define TEST 1 */
+#ifdef TEST
+#define TILES_HEIGHT "10"
+#define TILES_WIDTH  "12"
+#else
+#define TILES_HEIGHT "98242"
+#define TILES_WIDTH  "98147"
+#endif 
+
+/* int tile_floor[TILES_HEIGHT][TILES_WIDTH] = {0}; */
+
+#ifdef TEST
+#define MAX_RED_TILES 8
+#else
+#define MAX_RED_TILES 496
+#endif
+typedef struct Tile { int i,j; } Tile;
+
+long tile_count = 0;
+Tile tiles[MAX_RED_TILES] = {0};
+
+void print_tiles_to_file_svg(const char *filename) {
+    FILE *fp = fopen(filename, "wb");
+    fprintf(fp, "<svg width=\"" TILES_WIDTH "\" height=\"" TILES_HEIGHT"\" xmlns=\"http://www.w3.org/2000/svg\">\n");
+    for (int i=0; i < tile_count; i++) {
+        fprintf(fp, "\t<rect x=\"%d\" y=\"%d\" width=\"100\" height=\"100\" fill=\"black\" />\n", tiles[i].j, tiles[i].i);
+    }
+    fprintf(fp, "</svg>\n");
+    fclose(fp);
+}
+
+int main() {
+    #ifdef TEST
+    FILE *fp = fopen("day9input_small.txt", "rb");
+    #else 
+    FILE *fp = fopen("day9input.txt", "rb");
+    #endif
+
+    int i,j;
+    while (fscanf(fp, "%d,%d\n", &i, &j) != EOF) {
+        tiles[tile_count++] = (Tile){.i = j, .j = i};
+    }
+
+    /* print_tiles_to_file_svg("day9output.svg"); */
+
+    long max_area = 0;
+    for (int i=0; i<tile_count; i++) {
+        for (int j=i+1; j<tile_count; j++) {
+            int l1, l2;
+            if (tiles[i].i < tiles[j].i) { l1 = tiles[j].i - tiles[i].i; }
+            else { l1 = tiles[i].i - tiles[j].i; }
+
+            if (tiles[i].j < tiles[j].j) { l2 = tiles[j].j - tiles[i].j; }
+            else { l2 = tiles[i].j - tiles[j].j; }
+
+            long area = (long)(l1 + 1) * (long)(l2 + 1);
+            if (area > max_area) { max_area = area; }
+        }
+    }
+
+    printf("Part 1: %ld\n", max_area);
+    // 4752484112
+}
